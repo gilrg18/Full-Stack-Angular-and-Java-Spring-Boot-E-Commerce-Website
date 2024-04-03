15659f32ec79b54f46c26c7e54796c7943e6ed8a
gilberto.rogel <gilberto.rogel@aforecoppel.com>
2024-04-03 12:28:15 -0700

    Defining variables

---
+++ typescript-training/02-variables/sample-types.js
@@ -0,0 +1,7 @@
+var found = true;
+var grade = 9.99;
+var firstName = "Gil";
+var lastName = 'Rogel';
+console.log(found);
+console.log("Your grade is ".concat(grade));
+console.log("Hello ".concat(firstName, " ").concat(lastName));
---
+++ typescript-training/02-variables/sample-types.ts
@@ -0,0 +1,8 @@
+let found: boolean = true;
+let grade: number = 9.99;
+let firstName: string = "Gil";
+let lastName: string = 'Rogel';
+
+console.log(found);
+console.log(`Your grade is ${grade}`);
+console.log(`Hello ${firstName} ${lastName}`);
