fe6483f189e01559fa9b0958e99f1af5e52356a8
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-04-03 21:50:56 -0700

    Creating a new Angular Component Part 2

--- angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Gil","Ber","gil.ber@to.com",1818),
+    new SalesPerson("Otre","Blig","otre.blig@ot.com",444),
+    new SalesPerson("Mi","Guel","mi.guel@an.com",6666),
+    new SalesPerson("cle","men","cle.men@ton.com",42069),
+  ]
 
   constructor() { }
 
