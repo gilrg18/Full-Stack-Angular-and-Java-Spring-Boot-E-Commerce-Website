40837b0019bf4ec8b39b8994165fb023062d021b
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-04-04 16:56:27 -0700

    Creating Product class and product Service - Part 2

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Product } from '../common/product';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  private baseUrl = 'http://localhost:8080/api/products';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getProductList(): Observable<Product[]> {
+    //Returns an observable, Map the JSON data from Spring Data REST to Product array
+    return this.httpClient
+      .get<GetResponse>(this.baseUrl)
+      .pipe(map((response) => response._embedded.products));
+  }
+}
+
+//Unwraps the JSON from Spring Data REST _embedded entry
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  };
 }
