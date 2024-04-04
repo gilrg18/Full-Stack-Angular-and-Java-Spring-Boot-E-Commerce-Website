e61210847b3fe2e2b4291a97642c5d9c37bdf768
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-04-03 17:18:21 -0700

    Inheritance - Developing a second subclass

--- typescript-training/05-inheritance/Driver.ts
+++ typescript-training/05-inheritance/Driver.ts
@@ -1,4 +1,5 @@
 import { Circle } from "./Circle";
+import { Rectangle } from "./Rectangle";
 import { Shape } from "./Shape";
 
 let myShape = new Shape(10, 15);
@@ -7,3 +8,5 @@ console.log(myShape.getInfo());
 let myCircle = new Circle(5,10,20);
 console.log(myCircle.getInfo());
 
+let myRectangle = new Rectangle(0, 0, 3, 7);
+console.log(myRectangle.getInfo());
