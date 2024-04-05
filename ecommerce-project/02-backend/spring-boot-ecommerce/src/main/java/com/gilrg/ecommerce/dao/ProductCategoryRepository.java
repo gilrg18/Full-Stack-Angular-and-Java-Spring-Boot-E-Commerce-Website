4da86e08ac66af8a4ebc87af7051f6ea61384cb2
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-04-04 17:14:37 -0700

    Adding Cross Origin Support for Spring Boot App (CORS error fix)

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/gilrg/ecommerce/dao/ProductCategoryRepository.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/gilrg/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,10 @@ package com.gilrg.ecommerce.dao;
 import com.gilrg.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 //name of json entry="productCategory", path=/product-category
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path ="product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/gilrg/ecommerce/dao/ProductRepository.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/gilrg/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,9 @@ package com.gilrg.ecommerce.dao;
 
 import com.gilrg.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 //JpaRepository<Entity, Primary key>
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
