7caadd26e6361610d2f81b08700f60373b09bfbf
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-04-05 12:04:34 -0700

    Defining routes

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -6,12 +6,24 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 
+import {Routes, RouterModule} from '@angular/router';
+const routes: Routes = [
+  //Path to match , when path matches - create a new instance of component
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category/', component: ProductListComponent},
+  {path: 'products/', component: ProductListComponent},
+  //When the path is empty, match the FULL path not just the prefix(default pathMatch value)
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  //generic wildcard ** if it doesnt match any of the above routes
+  {path: '**', redirectTo: '/products', pathMatch: 'full'},
+]
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent
   ],
   imports: [
+    
     BrowserModule,
     HttpClientModule
   ],
