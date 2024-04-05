1d5f9e88fa6d1b0281891f4ac9680a85890eb38f
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-04-05 12:38:17 -0700

    Updating REST repository with new method to find products by category id

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/gilrg/ecommerce/dao/ProductRepository.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/gilrg/ecommerce/dao/ProductRepository.java
@@ -1,10 +1,21 @@
 package com.gilrg.ecommerce.dao;
 
 import com.gilrg.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 //JpaRepository<Entity, Primary key>
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    //findBy(querymethod), CategoryId(match by category id), @Param("id")(Use this parameter value)
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+    //Behind the Scenes, Spring will execute a query similiar to:
+    //SELECT * FROM product where category_id = ?
+    //Spring Data Rest automatically exposes endpoint:
+    //http://localhost:8080/api/products/search/findByCategoryId?id=?
+    //Also, the "/search" is provided by Spring Data REST.
+    // By default, any finder method defined in the repository will be exposed as an API.
 }
