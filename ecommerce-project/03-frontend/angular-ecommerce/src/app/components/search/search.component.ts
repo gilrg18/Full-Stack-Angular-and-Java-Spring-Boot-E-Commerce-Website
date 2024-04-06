db6ac2ddfc9a95b71281b218887546887330bfbd
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-04-05 17:04:06 -0700

    Send data to search route

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/search/search.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  //Inject the router into this component
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  doSearch(value: string){
+    console.log(`value= ${value}`);
+    //{path: 'search/:keyword', component: ProductListComponent}
+    this.router.navigateByUrl(`/search/${value}`);
   }
 
 }
