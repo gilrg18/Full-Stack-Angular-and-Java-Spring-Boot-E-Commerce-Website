09f834b68041deadcee180ad55547a8a8f996187
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-04-08 17:51:57 -0700

    Shopping cart status - Calling the CartService

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -25,7 +27,9 @@ export class ProductListComponent implements OnInit {
 
   //Inject our product service to this component, Inject the ActivatedRoute
   //The current active route that loaded the component. Useful for accessing route parameters
-  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+  constructor(private productService: ProductService, private route: ActivatedRoute,
+    private cartService: CartService
+  ) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(()=>{
@@ -116,5 +120,7 @@ export class ProductListComponent implements OnInit {
   //Update productlistcomponent with the click handler
   addToCart(theProduct: Product){
     console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+    const theCartItem = new CartItem(theProduct);
+    this.cartService.addToCart(theCartItem);
   }
 }
