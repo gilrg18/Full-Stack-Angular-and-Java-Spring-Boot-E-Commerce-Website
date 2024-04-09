7818234478bce46d21c53042108080b56b84fd48
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-04-09 10:22:13 -0700

    Shopping cart status - Add to cart from details view

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -9,12 +11,14 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
+
   //Had to initialize product to avoid error
   product: Product = new Product(1,'1','Not Found!','1',1,'',true,1,new Date(),new Date());
 
   constructor(
     private productService: ProductService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private cartService: CartService
   ) {}
 
   ngOnInit(): void {
@@ -34,4 +38,11 @@ export class ProductDetailsComponent implements OnInit {
       }
     )
   }
+
+  addToCart() {
+    console.log(`Adding to cart: ${this.product.id}, ${this.product.name}, ${this.product.unitPrice}` );
+    const theCartItem = new CartItem(this.product)
+    this.cartService.addToCart(theCartItem);
+  }
+
 }
