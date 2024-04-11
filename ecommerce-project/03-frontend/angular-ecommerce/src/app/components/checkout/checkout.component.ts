70ec9751b5959251edea85d99240435138c063a2
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-04-11 11:31:27 -0700

    Checkout Form Layout - Shipping Address

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -4,13 +4,12 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+  styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent implements OnInit {
-
   checkoutFormGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
@@ -19,16 +18,36 @@ export class CheckoutComponent implements OnInit {
         //key:[initial value]
         firstName: [''],
         lastName: [''],
-        email: ['']
-      })
-    })
+        email: [''],
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        county: [''],
+        zipCode: [''],
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: [''],
+      }),
+    });
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log('Handling the submit button');
     console.log(this.checkoutFormGroup.get('customer')!.value);
     console.log('Email: ', this.checkoutFormGroup.get('customer')!.value.email);
-
   }
-
 }
