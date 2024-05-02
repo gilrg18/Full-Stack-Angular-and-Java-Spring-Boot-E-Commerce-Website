be205a4807602123392670d6a60ae57a50ecff2e
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-05-02 15:27:01 -0700

    Refactoring - Backend configs - Unexposing customer data endpoint

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/gilrg/ecommerce/dao/CustomerRepository.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/gilrg/ecommerce/dao/CustomerRepository.java
@@ -4,6 +4,8 @@ package com.gilrg.ecommerce.dao;
 import com.gilrg.ecommerce.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//CustomerRepository is NOT annotated, hence it will NOT be exposed as REST API based on our configurations
+//Since @RepositoryRestResource is missing, trying to access this endpoint will show 404 not found 
 public interface CustomerRepository extends JpaRepository <Customer, Long> {
 
     Customer findByEmail(String theEmail);
--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/gilrg/ecommerce/dao/ProductRepository.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/gilrg/ecommerce/dao/ProductRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 //JpaRepository<Entity, Primary key>
+@RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product, Long> {
     //findBy(querymethod), CategoryId(match by category id), @Param("id")(Use this parameter value)
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
