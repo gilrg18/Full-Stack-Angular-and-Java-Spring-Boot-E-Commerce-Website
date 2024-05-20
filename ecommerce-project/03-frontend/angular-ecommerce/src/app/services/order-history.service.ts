185beed5a9a9e92bb882014e344e310a1dbe7bcb
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-05-20 10:08:17 -0700

    Develop OrderHistory Service

---
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/services/order-history.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { OrderHistory } from '../common/order-history';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderHistoryService {
+
+  private orderUrl = 'http://localhost:8080/api/orders';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getOrderHistory(theEmail: string): Observable<GetResponseOrderHistory>{
+    //build URL based on the customer email
+    const orderHistoryUrl = `${this.orderUrl}/search/findByCustomerEmail?email=${theEmail}`;
+
+    return this.httpClient.get<GetResponseOrderHistory>(orderHistoryUrl); // call the rest api
+  }
+}
+
+interface GetResponseOrderHistory{
+  _embedded: { //Unwraps the JSON from Spring Data REST _embedded entry
+    orders: OrderHistory[];
+  }
+}
